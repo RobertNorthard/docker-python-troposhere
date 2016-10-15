347757d397ba7820a8b5f14ac0eb8add5e4f5351
Robert Northard <robertnorthard@googlemail.com>
2016-10-15 19:45:08 +0100

    tabs

--- test/spec/localhost/Dockerfile_build_spec.rb
+++ test/spec/localhost/Dockerfile_build_spec.rb
@@ -4,16 +4,16 @@ require 'docker'
 describe "Dockerfile#build" do
 
 
-	before(:all) do
-		@docker_image = Docker::Image.build_from_dir('.')
+  before(:all) do
+    @docker_image = Docker::Image.build_from_dir('.')
     set :backend, :docker
-		set :os, family: :debian
-		set :docker_image, @docker_image.id		
-	end
+    set :os, family: :debian
+    set :docker_image, @docker_image.id   
+  end
 
-	describe command('python --version') do
-		its(:exit_status) { should eq 0 }
-		its(:stdout) { should eq "Python 3.4.2\n" }    
-	end
+  describe command('python --version') do
+    its(:exit_status) { should eq 0 }
+    its(:stdout) { should eq "Python 3.4.2\n" }    
+  end
 
 end
